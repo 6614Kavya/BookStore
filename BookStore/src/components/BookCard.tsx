41ec3a498dbbc20b71f74dbe07ad9acbfec0cf5f
6614Kavya <109873890+6614Kavya@users.noreply.github.com>
2024-12-28 14:15:57 +0530

    Implemented BookCard and rendered it using Flatlist

---
+++ BookStore/src/components/BookCard.tsx
@@ -0,0 +1,78 @@
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+
+interface BookCardProps {
+  title: string;
+  author: string;
+  description: string;
+  image?: string;
+}
+
+export const BookCard = ({
+  title,
+  author,
+  description,
+  image,
+}: BookCardProps) => {
+  return (
+    <View style={styles.bookItem}>
+      {image && <Image source={{uri: image}} style={styles.bookImage} />}
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.author}>By: {author}</Text>
+      <Text style={styles.description}>{description}</Text>
+      <View style={{flexDirection: 'row', justifyContent: 'flex-end'}}>
+        <TouchableOpacity style={styles.LogInButton}>
+          <Text style={styles.ButtonText}>Click me!</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  bookItem: {
+    marginBottom: 16,
+    padding: 16,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    borderWidth: 3,
+    borderColor: '#FFA621',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  author: {
+    fontSize: 14,
+    color: '#555',
+    marginTop: 4,
+  },
+  description: {
+    fontSize: 12,
+    color: '#777',
+    marginTop: 8,
+  },
+  bookImage: {
+    width: 80,
+    height: 120,
+    marginRight: 10,
+  },
+  LogInButton: {
+    backgroundColor: '#000080',
+    borderWidth: 3,
+    borderColor: '#FFA621',
+    padding: 12,
+    borderRadius: 8,
+    marginTop: 20,
+    alignItems: 'center', // Centers text inside the button
+  },
+  ButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
--- BookStore/src/screens/Home.tsx
+++ BookStore/src/screens/Home.tsx
@@ -1,6 +1,7 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import {BookCard} from '../components/BookCard';
 
 export const Home = () => {
   const [items, setItems] = useState([]);
@@ -29,10 +30,26 @@ export const Home = () => {
     console.log(items); // Log whenever items are updated
   }, [items]);
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <BookCard
+            title={item.title}
+            author={item.author}
+            description={item.description}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#000080',
+  },
+});
